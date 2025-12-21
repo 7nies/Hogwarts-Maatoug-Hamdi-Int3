7917177eb363ccc269f97c4f57830d3361d67639
7nies <ines.mtg92@gmail.com>
2025-12-21 17:12:03 +0100

    LIBERABLEEEEEEEEEE

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from menu import launch_menu_choice
 
 def main():
+    """
+    Main entry point of the program.
+    :return:
+    """
     launch_menu_choice()
 
 if __name__ == "__main__":
--- menu.py
+++ menu.py
@@ -3,7 +3,21 @@ from chapters.chapter_2 import start_chapter_2
 from chapters.chapter_3 import start_chapter_3
 from utils.input_utils import ask_choice
 
+
+def display_main_menu():
+    """
+    This function displays the main menu and gets player choice.
+    :return: str
+    """
+    choice = ask_choice("Do you want to start your adventure?   ",
+               ["Yes", "No, exit the game"])
+    return choice
+
 def launch_menu_choice():
+    """
+    Controls the main menu loop and orchestrates the game's progression.
+    :return:
+    """
     hogwarts_ascii = """
 
                                       #.                                                                 
@@ -39,18 +53,37 @@ def launch_menu_choice():
 
     """
     print(hogwarts_ascii)
-    choice = ask_choice(
-        "Do you want to start your adventure?   ",
-        ["Yes", "No"]
-    )
-
+    choice = display_main_menu()
     if choice == "Yes":
+        print()
+        print("Your magical journey begins...")
+        print()
+        input("Press Enter to continue...")
+        print()
+
         houses = {
             "Gryffindor": 0,
             "Slytherin": 0,
             "Hufflepuff": 0,
             "Ravenclaw": 0
         }
+
         char = start_chapter_1()
         char = start_chapter_2(char)
         start_chapter_3(char, houses)
+
+        # Texte de fin
+        print()
+        print("=" * 80)
+        print("              Thank you for playing! :3")
+        print("                     This game has been made by Ines Maatoug and Anas Hamdi ^_^")
+        print("=" * 80)
+        print()
+
+    else:
+        print()
+        print("Perhaps another time, then.")
+        print()
+        print("The magical world will be waiting when you're ready...")
+        print()
+        exit()
